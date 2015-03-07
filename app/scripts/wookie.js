0b35a32cbb7ae14f10cc808bae01da0537fd94d2
zacksimon <zacksimon.designer@gmail.com>
2015-03-06 23:25:20 -0500

    Add3d things and renamed them.

--- app/scripts/wookie.js
+++ app/scripts/wookie.js
@@ -1,4 +1,41 @@
+var s = Snap(1920,1080);
+
 // Wookie Effects
+//Draw wookie parts on the screen
+var wookieParts = [
+{
+	url: 'img/wookie-feet.svg',
+	svgName: "wookieFeet"
+},
+{
+	url: 'img/wookie-top-red.svg',
+	svgName: 'wookieTopRed'
+},
+{
+	url: 'img/wookie-top-blue.svg',
+	svgName: 'wookieTopBlue'
+},
+{
+	url: 'img/bg-stars.svg',
+	svgName: 'bgStars'
+}
+];
+
+
+function loadPart(part) {
+	Snap.load(part.url, function(fragment) {
+		var svgName = fragment.select("#" + part.svgName);
+		s.append(svgName);
+	});
+}
+
+wookieParts.forEach(function (part) {
+	console.log(part.url + ': ' + part.svgName);
+});	
+
+// for (i=0; i < wookieParts.length; i++){
+// };
+//Align wookie top and bottoms properly
 	// Bounce
 		// Whole wookie top slides down
 		// Whole wookie comes back up
@@ -11,3 +48,17 @@
 	// Special Effect
 		// Wookie top half slides up
 		// Confetti animation
+
+
+// var generateSvgFile = function(fragment) {
+// 	var innerGroup = fragment.select("g");
+// 	g.append(innerGroup);
+// }
+
+
+
+
+
+
+
+// Snap.load("img/wookie-top-red.svg", generateSvgFile);
