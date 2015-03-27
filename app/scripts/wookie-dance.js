399dfdb0b0ba9610a7113a86fd037ca73f22185a
Jeff Heaton <jeff@jgheaton.com>
2015-03-26 21:52:36 -0400

    Fleshed out dancing animations

--- app/scripts/wookie-dance.js
+++ app/scripts/wookie-dance.js
@@ -2,6 +2,14 @@ var danceLeft = function (){
 	$('#wookie-top-blue').addClass('left-dance');
 };
 
-var danceRight = function(){};
-var bounce = function();
-var shake = function();
+var danceRight = function(){
+	$('#wookie-top-blue').addClass('right-dance');
+};
+
+var bounce = function(){
+	$('#wookie-top-blue').addClass('bounce');
+};
+
+var shake = function(){
+	$('#wookie-top-blue').addClass('shake');
+};
